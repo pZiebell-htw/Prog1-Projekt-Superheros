d664f6299183e68902741c1394ffa686ef979115
pZiebell <philipp.ziebell.uni@gmail.com>
2024-12-18 13:06:13 +0100

    meister

--- testen.java
+++ testen.java
@@ -2,5 +2,6 @@ public class testen {
     public static void main(String[] args) {
         System.out.println("hello world");
         int a = 5;
+        System.out.println("Moin meister");
     }
 }
