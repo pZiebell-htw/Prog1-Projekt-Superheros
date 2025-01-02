4bfc71ce47de20a8ddd7b12a9ad8e6dc4394280a
pZiebell <philipp.ziebell.uni@gmail.com>
2025-01-02 15:02:34 +0100

    mainMenu

--- entwurfSpiel.java
+++ entwurfSpiel.java
@@ -3,35 +3,40 @@ public class entwurfSpiel {
         System.out.println("______________________________________________");
         System.out.println("");
         System.out.println("Superheroes vs. IT-Giant 1.0");
-        System.out.println("(1)  Create Superhero");
-        System.out.println("(2)  Show Selected Superhero");
-        System.out.println("(3)  List All Superhero");
-        System.out.println("(4)  Delete Superhero");
-        System.out.println("(5)  Start Raid");
-        System.out.println("(6)  Quit");
-        System.out.println("Please choose a number between 1 und 6:");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("(1)     Create Superhero");
+        System.out.println("(2)     Show Selected Superhero");
+        System.out.println("(3)     List All Superhero");
+        System.out.println("(4)     Delete Superhero");
+        System.out.println("(5)     Start Raid");
+        System.out.println("(6)     Quit");
+        System.out.println(" ");
+        System.out.println("Please pick a number between 1 und 6:");
         System.out.println("______________________________________________");
         System.out.println("______________________________________________");
 
-        System.out.println("(1)  Create Superhero");
-        System.out.println("");
+        System.out.println("---- Create Superhero ----");
+        System.out.println(" ");
         System.out.println("What would you like your hero to be called?");
-        System.out.println("Superhero1");
+        System.out.println("--UserInput(Name)--");
         System.out.println("______________________________________________");
 
         //Enter
-        System.out.println("");
+        System.out.println(" ");
         System.out.println("------ Your Superhero is created! ------");
-        System.out.println("");
         System.out.println("______________________________________________");
         System.out.println("______________________________________________");
 
-        System.out.println("(2)  Show Selected Superhero");
+        System.out.println("---- Show Selected Superhero ----");
         System.out.println(" ");
         System.out.println("Choose Superhero:");
-        System.out.println("(1)  Superheld.1(Name)");
-        System.out.println("(2)  Superheld.2(Name)");
-        System.out.println("(3)  Superheld.3(Name)");
+        System.out.println("(1)     Superheld.1(Name)");
+        System.out.println("(2)     Superheld.2(Name)");
+        System.out.println("(3)     Superheld.3(Name)");
+        System.out.println(" ");
+        System.out.println("Please pick a number between 1 and 3");
+
         System.out.println("______________________________________________");
 
         System.out.println("Superheld.1(Name):");
@@ -47,7 +52,7 @@ public class entwurfSpiel {
         System.out.println("______________________________________________");
         System.out.println("______________________________________________");
         
-        System.out.println("(3)  List All Superhero");
+        System.out.println("---- List All Superhero ----");
         System.out.println(" ");
         System.out.println("Superheld.1(Name)");
         System.out.println("Superheld.2(Name)");
@@ -55,18 +60,61 @@ public class entwurfSpiel {
         System.out.println("______________________________________________");
         System.out.println("______________________________________________");
 
-        System.out.println("(4)  Delete Superhero");
+        System.out.println("---- Delete Superhero ----");
         System.out.println(" ");
         System.out.println("Choose Superhero to be deleted:");
-        System.out.println("(1)  Superheld.1(Name)");
-        System.out.println("(2)  Superheld.2(Name)");
-        System.out.println("(3)  Superheld.3(Name)");
+        System.out.println("(1)     Superheld.1(Name)");
+        System.out.println("(2)     Superheld.2(Name)");
+        System.out.println("(3)     Superheld.3(Name)");
+        System.out.println(" ");
+        System.out.println("Please pick a number between 1 and 3");
         System.out.println("______________________________________________");
         System.out.println("Are you sure you want to delete"+"Superhero.1(Name)");
-        System.out.println("(1)Yes"+"(2)No");
+        System.out.println("(1)Yes"+"    "+"(2)No");
         System.out.println("______________________________________________");
         System.out.println("------"+ "Superhero.1(Name)"+ "is deleted! ------");
         System.out.println("______________________________________________");
         System.out.println("______________________________________________");
+
+        System.out.println("---- Raid Menu ----");
+        System.out.println(" ");
+        System.out.println("(1)     Explore");
+        System.out.println("(2)     Dance");
+        System.out.println("(3)     Quit Raid");
+        System.out.println(" ");
+        System.out.println("Please pick a number between 1 and 3");
+        System.out.println("______________________________________________");
+
+        System.out.println("---- Explore Raid ----");
+        System.out.println(" ");
+        System.out.println("Your team has detected an enemy. Which superhero do you want to choose for this quest?");
+        System.out.println("(1)     Fight");
+        System.out.println("(2)     Challange Enemy");
+        System.out.println("______________________________________________");
+
+        System.out.println("---- Dance ----");
+        System.out.println(" ");
+        System.out.println("Choose a Superhero to dance with:");
+        System.out.println("(1)     Superheld.1(Name)");
+        System.out.println("(2)     Superheld.2(Name)");
+        System.out.println("(3)     Superheld.3(Name)");
+        System.out.println(" ");
+        System.out.println("Please pick a number between 1 and 3");
+        System.out.println("______________________________________________");
+
+        System.out.println(" ");
+        System.out.println("Superhero.1(Name)"+"has started dancing");
+        System.out.println("______________________________________________");
+
+        System.out.println("---- Quit Raid ----");
+        System.out.println(" ");
+        System.out.println("Are you sure you want to quit the Raid?");
+        System.out.println("(1)Yes"+"    "+"(2)No");
+        System.out.println("______________________________________________");
+
+        System.out.println("You have successfully quit the Raid!");
+        System.out.println("______________________________________________");
+        System.out.println("______________________________________________");
+
     }
 }
