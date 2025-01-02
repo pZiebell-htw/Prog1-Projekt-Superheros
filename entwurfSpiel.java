494ae0e0c3a972d45347bff1e1074471074b89af
dkotiash <s0596635@htw-berlin.de>
2025-01-02 14:12:10 +0100

    (1) CreateSuperhero

--- entwurfSpiel.java
+++ entwurfSpiel.java
@@ -9,6 +9,7 @@ public class entwurfSpiel {
         System.out.println("(6)  Quit");
         System.out.println("Please choose a number between 1 und 6:");
         System.out.println("______________________________________________");
+        System.out.println("");
 
         System.out.println("(2)  Show Selected Superhero");
         System.out.println(" ");
@@ -17,6 +18,7 @@ public class entwurfSpiel {
         System.out.println("(2)  Superheld.2(Name)");
         System.out.println("(3)  Superheld.3(Name)");
         System.out.println("______________________________________________");
+        System.out.println("");
 
         System.out.println("Superheld.1(Name):");
         System.out.println(" ");
@@ -28,10 +30,22 @@ public class entwurfSpiel {
         System.out.println("readyToFight: ...");
         System.out.println("inFight: ...");
         System.out.println("alive: ...");
+        System.out.println("______________________________________________");
+        System.out.println("");
 
         //(1)  Create Superhero
+        System.out.println("");
 
         System.out.println("(1)  Create Superhero");
-        System.out.println("(1)  Create Superhero");
+        System.out.println("");
+        System.out.println("What would you like your hero to be called?");
+        System.out.println("Superhero1");
+        System.out.println("______________________________________________");
+
+        //Enter
+        System.out.println("");
+        System.out.println("------ Your Superhero is created! ------");
+        System.out.println("");
+        System.out.println("______________________________________________");
     }
 }
