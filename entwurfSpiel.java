d5bfb473b7df6348961b7969bb8577e87a197cf9
pZiebell <philipp.ziebell.uni@gmail.com>
2025-01-02 13:56:42 +0100

    ShowSelectedSuperhero

--- entwurfSpiel.java
+++ entwurfSpiel.java
@@ -7,5 +7,25 @@ public class entwurfSpiel {
         System.out.println("(4)  Delete Superhero");
         System.out.println("(5)  Start Raid");
         System.out.println("(6)  Quit");
+        System.out.println("______________________________________________");
+
+        System.out.println("(2)  Show Selected Superhero");
+        System.out.println(" ");
+        System.out.println("Choose Superhero:");
+        System.out.println("(1)  Superheld.1(Name)");
+        System.out.println("(2)  Superheld.2(Name)");
+        System.out.println("(3)  Superheld.3(Name)");
+        System.out.println("______________________________________________");
+
+        System.out.println("Superheld.1(Name):");
+        System.out.println(" ");
+        System.out.println("id: ...");
+        System.out.println("energyPointsCurrent: ...");
+        System.out.println("energyPointsMax: ...");
+        System.out.println("experiencePoints: ...");
+        System.out.println("lives: ...");
+        System.out.println("readyToFight: ...");
+        System.out.println("inFight: ...");
+        System.out.println("alive: ...");
     }
 }
