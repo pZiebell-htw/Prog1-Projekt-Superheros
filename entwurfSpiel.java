45b5f359e6acee72c472b1ad1e3997afbf7f78b8
dkotiash <s0596635@htw-berlin.de>
2025-01-02 13:41:26 +0100

    hauptmenu

--- entwurfSpiel.java
+++ entwurfSpiel.java
@@ -1,5 +1,11 @@
 public class entwurfSpiel {
     public static void main(String[] args) {
-        
+        System.out.println("Superheroes vs. IT-Giant 1.0");
+        System.out.println("(1)  Create Superhero");
+        System.out.println("(2)  Show Selected Superhero");
+        System.out.println("(3)  List All Superhero");
+        System.out.println("(4)  Delete Superhero");
+        System.out.println("(5)  Start Raid");
+        System.out.println("(6)  Quit");
     }
 }
---
+++ moqup.java
@@ -0,0 +1,5 @@
+public class moqup {
+    public static void main(String[] args) {
+        System.out.println("null");
+    }
+}
