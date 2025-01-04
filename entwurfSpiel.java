808f8b436cca192fa88db7c56e08e64cf3670770
dkotiash <s0596635@htw-berlin.de>
2025-01-04 15:52:55 +0100

    admin

--- entwurfSpiel.java
+++ entwurfSpiel.java
@@ -422,6 +422,63 @@ public class entwurfSpiel {
         System.out.println("______________________________________________");
         System.out.println("______________________________________________");
 
+        System.out.println("______________________________________________");
+        System.out.println("---- Admin ----");
+        System.out.println("You have "+" 3 "+ "attempts");
+        System.out.println("Please choose a number between 1 and 10:");
+        System.out.println("______________________________________________");
+
+        System.out.println("______________________________________________");
+        System.out.println("---- Admin ----");
+        System.out.println("The number is bigger!");
+        System.out.println("You have "+" 2 "+ "attempts");
+        System.out.println("Please choose a number between 1 and 10:");
+        System.out.println("______________________________________________");
+
+        System.out.println("______________________________________________");
+        System.out.println("---- Admin ----");
+        System.out.println("The number is smaller!");
+        System.out.println("You have "+" 2 "+ "attempts");
+        System.out.println("Please choose a number between 1 and 10:");
+        System.out.println("______________________________________________");
+
+        System.out.println("______________________________________________");
+        System.out.println("---- Admin ----");
+        System.out.println("The number is smaller!");
+        System.out.println("You have "+" 1 "+ "attempts");
+        System.out.println("Please choose a number between 1 and 10:");
+        System.out.println("______________________________________________");
+
+
+        System.out.println("______________________________________________");
+        System.out.println("----------- YOU GUESSED THE NUMBER! -----------");
+        System.out.println(" ");
+        System.out.println("Now you have a new artifact!");
+        System.out.println(" ");
+        System.out.println("                                 _       \n" + //
+                        "                                | |      \n" + //
+                        "  ___ ___  _ __   __ _ _ __ __ _| |_ ___ \n" + //
+                        " / __/ _ \\| '_ \\ / _` | '__/ _` | __/ __|\n" + //
+                        "| (_| (_) | | | | (_| | | | (_| | |_\\__ \\\n" + //
+                        " \\___\\___/|_| |_|\\__, |_|  \\__,_|\\__|___/\n" + //
+                        "                  __/ |                  \n" + //
+                        "                 |___/ ");
+        System.out.println(" ");
+        System.out.println("______________________________________________");
+
+        System.out.println("---- Admin defeted"+" Superhero.1(Name)"+"----");
+        System.out.println(" ");
+        System.out.println("     _       __           _   \n" + //
+                        "    | |     / _|         | |  \n" + //
+                        "  __| | ___| |_ ___  __ _| |_ \n" + //
+                        " / _` |/ _ \\  _/ _ \\/ _` | __|\n" + //
+                        "| (_| |  __/ ||  __/ (_| | |_ \n" + //
+                        " \\__,_|\\___|_| \\___|\\__,_|\\__|");
+        System.out.println(" ");                
+        System.out.println("Do you want to try again?");
+        System.out.println("(1)Yes"+"    "+"(2)No");                
+        System.out.println("______________________________________________");
+
         System.out.println(" ");
         System.out.println("---- Meet Enemy ----");
         System.out.println("Superhero"+"Superheld.1(Name)"+"has been selected for the quest and will fight against" +"BigBoss");
