4fa2387c73ea6c13586d748c1325894838233e8f
dkotiash <s0596635@htw-berlin.de>
2025-01-04 15:10:49 +0100

    bigBoss

--- entwurfSpiel.java
+++ entwurfSpiel.java
@@ -394,6 +394,46 @@ public class entwurfSpiel {
         System.out.println("______________________________________________");
         System.out.println(" ");
 
+        System.out.println("______________________________________________");
+        System.out.println("---- The Big Boss ----");
+        System.out.println("You start!");
+        System.out.println("Plese choose a column between 1 and 8:");
+
+
+        System.out.println("______________________________________________");
+        System.out.println("---- The Big Boss ----");
+        System.out.println("You chose 'number'");
+        System.out.println("Now it is the enemy's turn!");
+        System.out.println("To continue please press 1:");
+
+        System.out.println("______________________________________________");
+        System.out.println("---- The Big Boss ----");
+        System.out.println("You turn!");
+        System.out.println("Plese choose a column between 1 and 8:");
+
+
+        System.out.println("______________________________________________");
+        System.out.println("---- The Big Boss ----");
+        System.out.println("You turn!");
+        System.out.println("SPECIAL CHANCE");
+        System.out.println("Plese choose a column between 1 and 8:");
+        System.out.println("Plese choose a line between 1 and 8:");
+
+
+
+        System.out.println("______________________________________________");
+        System.out.println("---- The Big Boss ----");
+        System.out.println("CONGRATULATIONS! YOU WIN!");
+        System.out.println("Now you have a new artifact!");
+        System.out.println("______________________________________________");
+
+        System.out.println("______________________________________________");
+        System.out.println("---- The Big Boss ----");
+        System.out.println("YOU LOSE! :(");
+        System.out.println("Do you want to try again?");
+        System.out.println("(1)Yes"+"    "+"(2)No");
+        System.out.println("______________________________________________");
+
         System.out.println("---- Dance ----");
         System.out.println(" ");
         System.out.println("Choose a Superhero to dance with:");
@@ -420,6 +460,7 @@ public class entwurfSpiel {
         System.out.println("You have successfully quit the Raid!");
         System.out.println("______________________________________________");
         System.out.println("______________________________________________");
+
 
     }
 }
--- moqup.java
+++ moqup.java
@@ -1,5 +1,43 @@
 public class moqup {
     public static void main(String[] args) {
-        System.out.println("null");
+        System.out.println("______________________________________________");
+        System.out.println("---- The Big Boss ----");
+        System.out.println("You start!");
+        System.out.println("Plese choose a column between 1 and 8:");
+
+
+        System.out.println("______________________________________________");
+        System.out.println("---- The Big Boss ----");
+        System.out.println("You chose 'number'");
+        System.out.println("Now it is the enemy's turn!");
+        System.out.println("To continue please press 1:");
+
+        System.out.println("______________________________________________");
+        System.out.println("---- The Big Boss ----");
+        System.out.println("You turn!");
+        System.out.println("Plese choose a column between 1 and 8:");
+
+
+        System.out.println("______________________________________________");
+        System.out.println("---- The Big Boss ----");
+        System.out.println("You turn!");
+        System.out.println("SPECIAL CHANCE");
+        System.out.println("Plese choose a column between 1 and 8:");
+        System.out.println("Plese choose a line between 1 and 8:");
+
+
+
+        System.out.println("______________________________________________");
+        System.out.println("---- The Big Boss ----");
+        System.out.println("CONGRATULATIONS! YOU WIN!");
+        System.out.println("Now you have a new artifact!");
+        System.out.println("______________________________________________");
+
+        System.out.println("______________________________________________");
+        System.out.println("---- The Big Boss ----");
+        System.out.println("YOU LOSE! :(");
+        System.out.println("Do you want to try again?");
+        System.out.println("(1)Yes"+"    "+"(2)No");
+        System.out.println("______________________________________________");
     }
 }
